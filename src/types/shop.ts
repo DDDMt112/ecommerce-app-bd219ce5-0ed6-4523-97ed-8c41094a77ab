76595ea4e78372bb4c4f76ab9055d8e60087c10e
DDDMt112 <34934107+DDDMt112@users.noreply.github.com>
2025-04-25 11:07:30 +0800

    Add src/types/shop.ts

---
+++ src/types/shop.ts
@@ -0,0 +1,20 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  description: string;
+}
+
+export interface CartItem {
+  product: Product;
+  quantity: number;
+}
+
+export interface CartContextType {
+  items: CartItem[];
+  addToCart: (product: Product) => void;
+  removeFromCart: (productId: number) => void;
+  updateQuantity: (productId: number, quantity: number) => void;
+  total: number;
+}
